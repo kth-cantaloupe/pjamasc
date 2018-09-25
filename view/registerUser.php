de5067bff0eb3623771d1c052827f0d6c96420eb
PettaP <peters3@kth.se>
2018-09-25 11:29:46 +0200

    make forms for view

---
+++ view/registerUser.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PJAMASC</title>
+  </head>
+  <body>
+    <form class="rfp" action="newUser.php" method="POST">
+      <input type="text" name="company" placeholder="Company Name">
+      <input type="text" name="firstName" placeholder="First Name">
+      <input type="text" name="lastName" placeholder="Last Name">
+      <input type="text" name="tele" placeholder="Telephone number">
+      <input type="email" name="email" placeholder="Email Address">
+      <label>Choose Username</label>
+      <input type="text" name="userName" placeholder="Username">
+      <label>Choose Password</label>
+      <input type="text" name="password" placeholder="Password">
+      <input type="text" name="comfirmPassword" placeholder="Confirm Password">
+      <button type="submit" name="submit">Register User</button>
+    </form>
+  </body>
+</html>
