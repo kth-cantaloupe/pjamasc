08d5e1c402e360cbc12996ffe01c9972644b8058
Joey Öhman <joeyoh@kth.se>
2018-09-26 10:45:41 +0200

    Repair constans from Youssefs disaster

--- util/constants.php
+++ util/constants.php
@@ -1,19 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-$username = "username";
-$password = "password";
-$passwordConfirm = "confirmPassword";
-$email = "email";
-$error = "error";
-
-$companyName = "companyName";
-$firstName = "firstName";
-$lastName = "lastName";
-$phoneNumber = "phoneNumber";
-$message = "message";
-$file = "file";
-=======
 define("username", "username");
 define("password", "password");
 define("passwordConfirm", "passwordConfirm");
@@ -27,4 +13,3 @@ define("lastName", "lastName");
 define("message", "message");
 define("phoneNumber", "phoneNumber");
 define("file", "file");
->>>>>>> 5d292e526ffecbc25d06c01d014622e00367088a
